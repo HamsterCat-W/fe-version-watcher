b263c85d4f047cf04d04c1dc749f4c1bab8dc992
guoxuan.wang@transsion.com <61870782+HamsterCat-W@users.noreply.github.com>
2024-01-11 17:28:47 +0800

    chore: 产物路径修改

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { publishVersion } from '@lymtic/version-watcher-utils/dist/index.cjs'
+import { publishVersion } from '@lymtic/version-watcher-utils'
 
 export default defineConfig({
   plugins: [
