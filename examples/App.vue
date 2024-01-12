c01dbd3d9b0f731b8e86b18c940dc7bde4c0509b
guoxuan.wang@transsion.com <61870782+HamsterCat-W@users.noreply.github.com>
2024-01-12 16:50:09 +0800

    chore: demo例子

--- examples/App.vue
+++ examples/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div>test</div>
+    <div>tes2t</div>
     <FeVersionWatcher></FeVersionWatcher>
   </div>
 </template>
 
 <script setup lang="ts">
-import { FeVersionWatcher } from '@lymtic/version-watcher-components'
+import { FeVersionWatcher } from '@lymtic/version-watcher'
 </script>
--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { publishVersion } from '@lymtic/version-watcher-utils'
+import { publishVersion } from '@lymtic/version-watcher/vite'
 
 export default defineConfig({
   plugins: [
